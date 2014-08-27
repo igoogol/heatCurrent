868f854df88f9c98fd9e44b085530e7a5ec87239
googol <googolphys@gmail.com>
2014-08-27 17:48:43 +0800

    kT should be inside a pair of brackets.

--- heatCurrent/heatCond.py
+++ heatCurrent/heatCond.py
@@ -16,34 +16,34 @@ Gamma2 = 0.5
 Gamma = Gamma1 + Gamma2
 SigmaR = -1j*Gamma/2.0
 SigmaA = +1j*Gamma/2.0
-Omega = 0.1    # response frequency
-kT = 1.0       # 25.6  # room temperature
-E0 = 0.0       # single energy level of the QD
-NE = 2000      # number of energy points
-Ueq = 0.0      # equilibrium potential
+Omega = 2.0     # response frequency
+kT = 1.0        # 25.6  # room temperature
+E0 = 0.0        # single energy level of the QD
+NE = 2000       # number of energy points
+Ueq = 0.0       # equilibrium potential
  
 # Energy grid
-FermiEnergy = sp.linspace(-20,20,20)
+FermiEnergy = sp.linspace(-10,10,200)
 Gh = []
 for Ef in FermiEnergy: 
     Emin = -20
     A = 1
     while A > 1e-10:
         Emin = Emin - 10  
-        f = 1.0/(sp.exp(Emin-Ef)/kT + 1)
-        fBar = 1.0/(sp.exp(Emin+Omega-Ef)/kT + 1)
+        f = 1.0/(sp.exp((Emin-Ef)/kT) + 1)
+        fBar = 1.0/(sp.exp((Emin+Omega-Ef)/kT) + 1)
         A = (f-fBar)/Omega
     Emax = 20
     A = 1
     while A > 1e-10:
         Emax = Emax + 10  
-        f = 1.0/(sp.exp(Emax-Ef)/kT + 1)
-        fBar = 1.0/(sp.exp(Emax+Omega-Ef)/kT + 1)
+        f = 1.0/(sp.exp((Emax-Ef)/kT) + 1)
+        fBar = 1.0/(sp.exp((Emax+Omega-Ef)/kT) + 1)
         A = (f-fBar)/Omega    
     E = np.linspace(Emin,Emax,NE)
     dE = E[1] - E[0]
-    f = 1.0/(sp.exp(E-Ef)/kT + 1)
-    fBar = 1.0/(sp.exp(E+Omega-Ef)/kT + 1)
+    f = 1.0/(sp.exp((E-Ef)/kT) + 1)
+    fBar = 1.0/(sp.exp((E+Omega-Ef)/kT) + 1)
 #     # one way to declare 2-D matrix
 #     Gr = [[0 for col in range(1)]for row in range(NE)]
 #     Gr[2][0] = 3
@@ -79,7 +79,7 @@ plt.plot(FermiEnergy,sp.real(Gh),label='real',color='blue')
 plt.plot(FermiEnergy,sp.imag(Gh),label='imag',color='red')
 plt.xlabel("$E_f/kT$")
 plt.ylabel("$G^h$")
-plt.title("$\Omega = 1kT$")
+plt.title("$\Omega = 2 kT$, $\Gamma_L = \Gamma_R = 0.5 kT$")
 plt.legend()
 plt.show()
 
